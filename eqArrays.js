bbf034882e24ac4acfc93bcbcae5fdbaa4863294
nktrancs <nktran4@gmail.com>
2022-04-06 00:11:23 +0000

    eqArrays added

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} == ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function (a ,b) {
+  if (a.length === b.length) {
+    for (let i = 0; i < a.length; i++){
+      if (a[i] !== b[i]) {
+        return false 
+      }       
+    }
+    return true
+  } else {
+    return false
+  }
+}
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
