111dd4dbbfb0eff8645e81de18a1018adfc4b5a7
nktrancs <nktran4@gmail.com>
2022-04-08 00:11:46 +0000

    take until

---
+++ takeUntil.js
@@ -0,0 +1,21 @@
+const takeUntil = function(array, callback) {
+  const ans = [] 
+  for (let list of array) {
+    if (callback(list)) {
+      return ans;
+    } else {
+      ans.push(list)
+    }   
+  } 
+  return ans; 
+}; 
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
